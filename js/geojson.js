40d517f6403e5a2239b83d3dad7aae4831ad1fbf
meghankelly-cartography <mkelly22@wisc.edu>
2016-02-12 09:14:54 -0600

    Module 4 continued
    Next up, clustering!

---
+++ js/geojson.js
@@ -0,0 +1,46 @@
+/* Map of GeoJSON data from MegaCities.geojson */
+
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    var map = L.map('map', {
+        center: [20, 0],
+        zoom: 2
+    });
+
+    //add OSM base tilelayer
+	L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+    //call getData function
+    getData(map);
+};
+
+//function to retrieve the data and place it on the map
+function getData(map){
+    //Example 2.3 line 22...load the data
+    $.ajax("data/MegaCities.geojson", {
+        dataType: "json",
+        success: function(response){
+            //create marker options
+            var geojsonMarkerOptions = {
+                radius: 8,
+                fillColor: "#ff7800",
+                color: "#000",
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
+            };
+
+            //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(response, {
+                pointToLayer: function (feature, latlng){
+                    return L.circleMarker(latlng, geojsonMarkerOptions);
+                }
+            }).addTo(map);
+        }
+    });
+};
+
+$(document).ready(createMap);
