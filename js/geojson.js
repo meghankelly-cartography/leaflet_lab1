1d182fcd6466778a91441e0419e019c2cdb599b5
meghankelly-cartography <mkelly22@wisc.edu>
2016-02-24 09:09:54 -0600

    Module 5 continued
    I have the array, need to continue increment through data.

--- js/geojson.js
+++ js/geojson.js
@@ -16,6 +16,49 @@ function createMap(){
     getData(map);
 };
 
+//Create sequence control
+//This function will be called in the getData AJAX function
+function createSequenceControls(map){
+    //create range input element (slider)
+    $('#panel').append('<input class="range-slider" type="range">');
+    
+    //set slider attributes
+    $('.range-slider').attr({
+        max: 11,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+    
+    $('#panel').append('<button class="skip" id="reverse">Back</button>');
+    $('#panel').append('<button class="skip" id="forward">Forward</button>');
+};
+
+//Build array that houses attributes from data
+function processData(data){
+	
+	//create empty array to hold attributes, use square brackets
+	var attributes = [];
+	
+	//create variable of first feature in data
+	var properties = data.features[0].properties;
+	
+	//then push each attribute name into the array
+	for (var attribute in properties){
+		
+		//only take attributes with population values
+		if (attribute.indexOf("month") > -1){
+			attributes.push(attribute);
+		};
+	};
+	
+	//check result with console
+	console.log(attributes);
+	
+	return attributes;
+};
+
+
 //Step 2: Import GeoJSON data
 function getData(map){
     //load the data
@@ -23,11 +66,19 @@ function getData(map){
         dataType: "json",
         success: function(response){
         
-            console.log(response.features[1].properties)
-            console.log(response.features)
+        	//Test set by printing to console
+        	//I used these functions to begin querying my data in the console
+            //console.log(response.features[1].properties)
+            //console.log(response.features)
+
+			// create array of attributes
+			var attributes = processData(response);
 
             //call function to create proportional symbols
-            createPropSymbols(response, map);
+            createPropSymbols(response, map, attributes);
+            
+            //call function to create sequence control as a response to data AJAX call
+            createSequenceControls(map, attributes);
         }
     });
 };
@@ -37,7 +88,7 @@ $(document).ready(createMap);
 function createPropSymbols(data, map){
     
     // define variable that is an attribute or property
-    var attribute = "April";
+    var attribute = "month_April";
     
     //create marker options
     var geojsonMarkerOptions = {
@@ -74,6 +125,67 @@ function calcPropRadius(attValue) {
     var radius = Math.sqrt(area/Math.PI);
 
     return radius;
+};
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng){
+	//Determine which attribute to visualize with proportional symbols
+	var attribute = "month_April";
+	
+	//Create marker options
+	var options = {
+		fillColor: "blue",
+		color: "#000",
+		weight: 1,
+		opacity: 1,
+		fillOpacity: 0.6,
+	};
+	
+	//For each feature, determine its value for the selected attribute
+	var attValue = Number(feature.properties[attribute]);
+	
+	//Give each feature's circle marker a radius based on its attribute value
+	options.radius = calcPropRadius(attValue);
+	
+	//create circle marker layer
+	var layer = L.circleMarker(latlng, options);
+	
+	//build popup content string starting with city...Example 2.1 line 24
+    var popupContent = "<p><b>Country:</b> " + feature.properties.Country + "</p>";
+
+    //add formatted attribute to popup content string
+    var month = attribute.split("_")[1];
+    popupContent += "<p><b>Month:</b> " + attribute +"</p>";
+    
+    //add formatted attribute to popup content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Number of asylum seekers:</b> " + feature.properties[attribute] + "</p>";
+    
+    layer.bindPopup(popupContent, {
+        offset: new L.Point(0,-options.radius)
+    });
+
+    //event listeners to open popup on hover
+    layer.on({
+        mouseover: function(){
+            this.openPopup();
+        },
+        mouseout: function(){
+            this.closePopup();
+        }
+    });
+    
+	
+	//return the circle marker to the L.geojson pointToLayer option
+	return layer;
+};
+
+//Add Circle Markers for point features to map
+function createPropSymbols(data, map){
+	//Create a leaflet geojson layer and add it to the map
+	L.geoJson(data, {
+		pointToLayer: pointToLayer
+	}).addTo(map);
 };
 
 
