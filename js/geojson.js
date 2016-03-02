2f2afb24e1fe04ae03a3ca096066bc79b855682e
meghankelly-cartography <Student@SHC-380-001.shc.wisc.edu>
2016-03-02 17:11:07 -0600

    Module 5, minor edit

--- js/geojson.js
+++ js/geojson.js
@@ -12,43 +12,43 @@ function createMap(){
 
     //call getData function
     getData(map);
-    //getDataChoro(map);
+    getDataChoro(map);
 };
 
-// //Import GeoJSON data
-// function getDataChoro(map){
-//     //load the data with callback function
-//     $.ajax("data/europeData.geojson", {
-//         dataType: "json",
-//         success: function(response){
+//Import GeoJSON data
+function getDataChoro(map){
+    //load the data with callback function
+    $.ajax("data/europeData.geojson", {
+        dataType: "json",
+        success: function(response){
 
-//             choropleth = response;
-//            //create a Leaflet GeoJSON layer and add it to the map
-//             L.geoJson(choropleth, {style: style}).addTo(map);
-//             L.control.layers(choropleth).addTo(map);
-//         }
-//     });
-// };
+            choropleth = response;
+           //create a Leaflet GeoJSON layer and add it to the map
+            L.geoJson(choropleth, {style: style}).addTo(map);
+            L.control.layers(choropleth).addTo(map);
+        }
+    });
+};
 
-// function getColor(d) {
-//     return d > 10000000 ? '#08519c' :
-//            d > 5000000  ? '#3182bd' :
-//            d > 2000000  ? '#6baed6' :
-//            d > 1000000  ? '#bdd7e7' :
-//            d > 500000   ? '#eff3ff' :
-//                           'white';
-// }
+function getColor(d) {
+    return d > 10000000 ? '#08519c' :
+           d > 5000000  ? '#3182bd' :
+           d > 2000000  ? '#6baed6' :
+           d > 1000000  ? '#bdd7e7' :
+           d > 500000   ? '#eff3ff' :
+                          'white';
+}
 
-// function style(feature) {
-//     return {
-//         fillColor: getColor(feature.properties.pop_est),
-//         weight: 1,
-//         opacity: 1,
-//         color: 'white',
-//         dashArray: '3',
-//         fillOpacity: 0.6
-//     };
-// }
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.pop_est),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.6
+    };
+}
 
 
 
